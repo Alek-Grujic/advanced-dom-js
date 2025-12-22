8f9f4e200f9536b648c2733e00aedea09d9925fe
Alek-Grujic <alek.grujic001@outlook.com>
2025-12-22 23:45:34 +0100

    DOM traversing fundamentals

--- script.js
+++ script.js
@@ -59,14 +59,30 @@ btnScrollTo.addEventListener("click", function (e) {
 
 // Page navigation
 
-document.querySelectorAll(".nav__link").forEach((el) => {
-  el.addEventListener("click", function (e) {
-    e.preventDefault();
+// document.querySelectorAll(".nav__link").forEach((el) => {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
 
-    const id = this.getAttribute("href");
+//     const id = this.getAttribute("href");
+
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// better solution
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
 
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  });
+  }
 });
 
 /////////////////////////////////////////
@@ -183,3 +199,33 @@ document.querySelectorAll(".nav__link").forEach((el) => {
 //   this.style.backgroundColor = randomColor();
 //   console.log("Nav", e.target, e.currentTarget);
 // });
+
+// traversing
+
+const h1 = document.querySelector("h1");
+
+// going downwards: child
+
+console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// going upwards: parents
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+// going sideways: sibilings
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = "scale(0.5)";
+});
